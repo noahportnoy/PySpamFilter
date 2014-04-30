19afed61bf71a6b7f9df22eb1ee70c9b52cd78a7
acsousa <sousa57a@gmail.com>
2014-04-29 20:31:13 -0400

    Andrews updates

--- plot.py
+++ plot.py
@@ -30,55 +30,78 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-st", "--strain", help="Enter the spam directory you want to train on")
 parser.add_argument("-ht", "--htrain", help="Enter the ham directory you want to train on")
 parser.add_argument("-sd", "--spam_detect", help="Enter email that you would like to check the spam ranking of. Must be in PySpamFilter directory")
+parser.add_argument("-ta", "--test_accuracy", nargs= 2, help="Enter spam first and then ham email-directory that you would like to check the spam ranking of. Must be in PySpamFilter directory")
 parser.add_argument("-rw", "--rank_word", help="Rank this word more than others")
-parser.add_argument("-clc", "--clear_training", help="Clear out the training data", action="store_true")
 args = parser.parse_args()
+
+#---------- If spam / ham is not inputed then send training_ham, spam--------
+#Training HAM case
+ham_dir = None
+spam_dir = None
 
 #Training SPAM case
 if args.strain != None:
 	#Check to see if directory is in directory
 	if args.strain in os.listdir():
 		print("Spam training commencing!")
+		spam_dir = args.strain
 	else:
 		print("There is no folder '"+args.strain+"' in this directory.")
+		exit()
+else:
+	spam_dir = "training_spam"
 
-#Training HAM case
 if args.htrain != None:
 	#Check to see if directory is in directory
-	if args.train in os.listdir():
+	if args.htrain in os.listdir():
 		print("Ham training commencing!")
+		ham_dir = args.htrain
 	else:
 		print("There is no folder '"+args.htrain+"' in this directory.")
+		exit()
+else:
+	ham_dir = "training_ham"
+#----------------------------------------------------------------------
+training.train_spam_filter(spam_dir, ham_dir);
+
 
 #Spam Detect case
 if args.spam_detect != None:
 	if(os.path.isfile(args.spam_detect)) is True:
 		#Detect the spam of this email!!
 		print("Good to go")
+		spam_filter.spam_detect_email(args.spam_detect, "combined" , .0000000001 )
+		exit()
+	else:
+		print("There is no email '"+args.spam_detect+"' in this directory.")
 
 #Rank Word
 if args.rank_word != None:
-	print("Rank on this word")
-	#Rank on this particular word...
+	print("The ranking of  '"+args.rank_word+"' is:")
+	print(spam_filter.spam_rank(args.rank_word))
+	exit()
 
-#Clear our training data
-if args.clear_training == 1:
-	print("\nClearing training data....\n")
-	#Call clear training data method here
+#Detect accuracy of a directory case
+if args.test_accuracy[1] != None:
+	#Check if first (SPAM) and second (HAM) directorys are there
+	if ((args.test_accuracy[0] in os.listdir()) & ((args.test_accuracy[1] in os.listdir()))):
+		print("Folders are good")
+		print("There is no directory '"+args.test_accuracy[0]+"' or '"+args.test_accuracy[1]+"' in this directory.")
+
+		plot_mean = spam_filter.test_accuracy(args.test_accuracy[0], args.test_accuracy[1], "mean");
+		plot_majority = spam_filter.test_accuracy(args.test_accuracy[0], args.test_accuracy[1], "majority");
+		plot_combined = spam_filter.test_accuracy(args.test_accuracy[0], args.test_accuracy[1], "combined")
+
+		spam_plots = [(plot_mean, "mean"), (plot_majority, "majority"), (plot_combined, "naive Bayes")];
+		plot_detection_rates(spam_plots);
+
+	else:
+		print("There is no directory '"+args.test_accuracy[0]+"' or '"+args.test_accuracy[1]+"' in this directory.")
+	exit()
 
 
-exit()
-
-
-
-training.train_spam_filter("training_spam", "training_ham");
 # original
-plot_mean = spam_filter.test_accuracy("test_spam", "test_ham", "mean");
-plot_majority = spam_filter.test_accuracy("test_spam", "test_ham", "majority");
-plot_combined = spam_filter.test_accuracy("test_spam", "test_ham", "combined")
 
-spam_plots = [(plot_mean, "mean"), (plot_majority, "majority"), (plot_combined, "naive Bayes")];
-plot_detection_rates(spam_plots);
 
 
 # for presentation!
